a128dcf33c631c925f140a412138aad95c195885
Narcís Palomeras <narcispr@gmail.com>
2022-03-19 08:26:39 +0100

    Improved setup.py file

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ setuptools.setup(
         name="PyFreatboard",
         version="0.2",
         author="Narcis Palomeras",
-        description="PyFreatboard is an API for drawing diagrams (aka shapes) on the guitar freatboard using Python as well as to automatically generate new shapes.",
+        description="PyFreatboard is an API for drawing diagrams on the guitar freatboard.",
+        long_description="PyFreatboard is an API for drawing diagrams (aka shapes) on the guitar freatboard using Python as well as to automatically generate new shapes.",
+        long_description_content_type="text/markdown",
+        url="https://github.com/narcispr/PyFreatboard",
+        classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+        ],
         packages=["PyFreatboard"],
         install_requires=[
           'matplotlib',
